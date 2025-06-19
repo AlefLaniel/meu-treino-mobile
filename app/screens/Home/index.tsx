6dc20e1bb3190b699172fcc3a6cbfae8760f2333
AlefLaniel <alef6940@gmail.com>
2025-06-19 14:32:49 -0300

    fix: ajustar margens dos botões na tela inicial

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -310,7 +310,7 @@ const handleNextExercise = () => {
               {areAllExercisesCompleted() ? (
                 <Button
                   onPress={resetCompleted}
-                  className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                  className=" px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
                 >
                   <Text className="dark:text-white">Finalizar Exercícios</Text>
                 </Button>
@@ -319,7 +319,7 @@ const handleNextExercise = () => {
                   {/* Botão de próximo exercício */}
                   <Button
                     onPress={handleNextExercise}
-                    className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    className=" px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                   >
                     <CustomText className="dark:text-white">Próximo Exercício</CustomText>
                   </Button>
